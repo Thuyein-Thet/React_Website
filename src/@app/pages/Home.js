ef9a486784cbc3897da59d5ccde0fde1ed17c20c
Thuyein-Thet <thuyein.thet555@gmail.com>
2022-03-22 23:37:16 +0630

    Nav & Home

--- src/@app/pages/Home.js
+++ src/@app/pages/Home.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BannerImage from "../assests/pizza.jpeg";
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>Welcome To Pizza World!</div>
+    <div className='home'>
+        <div 
+        className="headerContainer" 
+        style={{backgroundImage: `url(${BannerImage})`}}
+        >
+            <h1>Italia's  Pizza</h1>
+            <p>Delicious!</p>
+            <Link to="/menu"><button>ORDER NOW</button></Link>
+        </div>
+    </div>
   )
 }
 
