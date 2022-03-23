7da09bbfcbe8146471ebbed7cd15c881da20514a
Thuyein-Thet <thuyein.thet555@gmail.com>
2022-03-23 14:05:49 +0630

    Complete

---
+++ src/@app/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PizzaLeft from '../assests/left.jpg'
+import '../styles/Contact.css';
+
+function Contact() {
+  return (
+    <div className='contact'>
+        <div className='leftSide' style={{backgroundImage: `url(${PizzaLeft})`}}>sdasdg</div>
+        <div className='rightSide'>
+            <h1>Contact us</h1>
+            <form id='contact-form' method='POST'>
+            <label htmlFor='name'>Full Name</label>
+            <input name='name' placeholder='Enter your full name' type='text'/>
+            <label htmlFor='email'>Email</label>
+            <input name='email' placeholder='Enter your email' type='email'/>
+            <label htmlFor='message'>Message</label>
+            <textarea name='message' placeholder='Enter your message' rows='6' required></textarea>
+            <button type='submit'>Send Message</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
